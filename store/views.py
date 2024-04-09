32ee352e27f945e8f1f6aaf9bb6bb9692e8098e4
agliullin13artem <150588030+agliullin13artem@users.noreply.github.com>
2024-04-09 22:42:40 +0300

    add permissions

--- store/views.py
+++ store/views.py
@@ -1,25 +1,32 @@
-
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 import django_filters.rest_framework
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.permissions import  IsAuthenticatedOrReadOnly
+
+
+from store.permissions import IsOwnerOrStaffOrReadOnly
 
 from .models import Book
 from .serializers import BooksSerializer
 
 
-
 class BooksViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BooksSerializer
-    filter_backends = [django_filters.rest_framework.DjangoFilterBackend, SearchFilter, OrderingFilter]
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    filterset_fields = ['price']
-    search_fields = ['name', 'author_name']
-    ordering_fields = ['price', 'author_name']
+    filter_backends = [
+        django_filters.rest_framework.DjangoFilterBackend,
+        SearchFilter,
+        OrderingFilter,
+    ]
+    permission_classes = [IsOwnerOrStaffOrReadOnly]
+    filterset_fields = ["price"]
+    search_fields = ["name", "author_name"]
+    ordering_fields = ["price", "author_name"]
+
+    def perform_create(self, serializer):
+        serializer.validated_data["owner"] = self.request.user
+        serializer.save()
 
 
 def auth(request):
-    return render(request, 'oauth.html')
- 
+    return render(request, "oauth.html")
