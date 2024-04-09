259e1c3a5feca4ce39061e457e28d83e54f101b2
agliullin13artem <150588030+agliullin13artem@users.noreply.github.com>
2024-04-09 21:32:13 +0300

    add fileds owner

--- store/models.py
+++ store/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 
 class Book(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     author_name = models.CharField(max_length=255, default='Author')
+    owner = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f'Id {self.id}: {self.name}'
@@ -13,3 +14,4 @@ class Book(models.Model):
     class Meta:
         verbose_name='Книга'
         verbose_name_plural = 'Книги'
+    
